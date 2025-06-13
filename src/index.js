8e9d9e936440c28990d53efe25542275668ec456
kylee-ingalls <kylee.ingalls@gmail.com>
2025-06-13 16:47:23 -0500

    Added basic form handling and added Typewriter plugin JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "A name is given, a name is earned, Through every lesson we have learned. It marks our place, both near and far— A quiet guide to who we are. It may be common, bold, or rare—But every name is made with care.",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
